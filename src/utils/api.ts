e0fa1b08bc1939313348a91fe45b6f6aed58d995
jay-bhogayata <jaybhogayata53@gmail.com>
2024-06-07 21:41:49 +0530

    chore: Update signup page styling and API endpoint

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  baseURL: process.env.NEXT_PUBLIC_API_URL + "/api/v1",
+  headers: {
+    "Access-Control-Allow-Origin": process.env.NEXT_PUBLIC_API_URL,
+  },
   withCredentials: true,
 });
 
