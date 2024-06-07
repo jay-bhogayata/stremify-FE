8de7c9466dbfaef34828fc150915526bf23df178
jay-bhogayata <jaybhogayata53@gmail.com>
2024-06-07 17:07:51 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Stremify
+
+- stremify is video streaming platform that allows users to watch videos online. this is front-end part of the project.
+
+## Getting Started
+
+
+```bash
+npm run dev
+# or
+yarn dev
+# or
+pnpm dev
+# or
+bun dev
+```
---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center  p-24">
+      <h1 className="text-5xl py-10 font-semibold text-purple-500">
+        welcome to stremify
+      </h1>
+      <p>coming soon...</p>
+    </main>
+  );
+}
